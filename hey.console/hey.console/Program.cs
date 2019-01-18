662a089d173353c6f7b1d3ab3cad43ea5eb569d1
Mosiur <mosiurmonna123@gmail.com>
2019-01-18 12:14:19 +0600

    adding initial project files

---
+++ hey.console/hey.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hey.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
